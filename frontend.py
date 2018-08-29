0f8549e59fbbba030bf6aa6bb4afff5531d8717d
thekaranjit <thekaranjit@gmail.com>
2018-08-29 04:18:03 +0400

    Completed Programming

--- frontend.py
+++ frontend.py
@@ -14,8 +14,22 @@ Close
 """
 from tkinter import *
 import backend
-import ctypes
-import time
+import ctypes # for message box
+
+
+def get_selected_row(event):
+    global selected_tuple
+    index=list1.curselection()[0]
+    selected_tuple=list1.get(index)
+    e1.delete(0,END)
+    e1.insert(END,selected_tuple[1])
+    e2.delete(0, END)
+    e2.insert(END,selected_tuple[2])
+    e3.delete(0, END)
+    e3.insert(END,selected_tuple[3])
+    e4.delete(0, END)
+    e4.insert(END,selected_tuple[4])
+
 
 
 def view_command():
@@ -35,9 +49,18 @@ def add_command():
     MessageBox = ctypes.windll.user32.MessageBoxW
     MessageBox(None, 'Entry Sucessfull', 'Sucess', 0)
 
+def delete_command():
+    backend.delete(selected_tuple[0])
+    MessageBox = ctypes.windll.user32.MessageBox
+    MessageBox(None, 'Entry Sucessfull Deleted', 'Sucess', 0)
+
+def update_command():
+    backend.update(selected_tuple[0],title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
 
 
 window = Tk()
+
+window.wm_title("BookStore")
 
 # Titles
 l1=Label(window, text="Title")
@@ -58,16 +81,16 @@ e1=Entry(window,textvariable=title_text)
 e1.grid(row=0, column=1)
 
 author_text=StringVar()
-e1=Entry(window,textvariable=author_text)
-e1.grid(row=0, column=3)
+e2=Entry(window,textvariable=author_text)
+e2.grid(row=0, column=3)
 
 year_text=StringVar()
-e1=Entry(window,textvariable=year_text)
-e1.grid(row=1, column=1)
+e3=Entry(window,textvariable=year_text)
+e3.grid(row=1, column=1)
 
 isbn_text=StringVar()
-e1=Entry(window,textvariable=isbn_text)
-e1.grid(row=1, column=3)
+e4=Entry(window,textvariable=isbn_text)
+e4.grid(row=1, column=3)
 
 # List Box
 
@@ -82,6 +105,7 @@ sb1.grid(row=2,column=2,rowspan=6)
 list1.configure(yscrollcommand=sb1.set)
 sb1.configure(command=list1.yview)
 
+list1.bind('<<ListboxSelect>>',get_selected_row)
 
 # Buttons
 
@@ -94,13 +118,13 @@ b2.grid(row=3, column=3)
 b3=Button(window,text="Add Entry", width=12,command=add_command)
 b3.grid(row=4, column=3)
 
-b4=Button(window,text="Update Selected", width=12)
+b4=Button(window,text="Update Selected", width=12,command=update_command)
 b4.grid(row=5, column=3)
 
-b5=Button(window,text="Delete Selected", width=12)
+b5=Button(window,text="Delete Selected", width=12,command=delete_command)
 b5.grid(row=6, column=3)
 
-b6=Button(window,text="Close", width=12)
+b6=Button(window,text="Close", width=12,command=window.destroy)
 b6.grid(row=7, column=3)
 
 
